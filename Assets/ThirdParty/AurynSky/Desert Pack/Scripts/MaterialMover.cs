09bff6ffe573c5c317738df683904aa2ad6bd398
Michal Louda <mloud@seznam.cz>
2025-04-04 14:42:51 +0200

    material mover commented - performance issues

--- Assets/ThirdParty/AurynSky/Desert Pack/Scripts/MaterialMover.cs
+++ Assets/ThirdParty/AurynSky/Desert Pack/Scripts/MaterialMover.cs
@@ -7,13 +7,13 @@ public class MaterialMover : MonoBehaviour
 
     public float scrollSpeed = 0.5F;
     public Renderer rend;
-    void Start()
-    {
-        rend = GetComponent<Renderer>();
-    }
-    void Update()
-    {
-        float offset = Time.time * scrollSpeed;
-        rend.material.SetTextureOffset("_MainTex", new Vector2(0, offset));
-    }
+    // void Start()
+    // {
+    //     rend = GetComponent<Renderer>();
+    // }
+    // // void Update()
+    // {
+    //     float offset = Time.time * scrollSpeed;
+    //     rend.material.SetTextureOffset("_MainTex", new Vector2(0, offset));
+    // }
 }
