28757466c901d5118ecd55ef3d921f1826dd6894
Michal Louda <mloud@seznam.cz>
2025-04-07 14:02:00 +0200

    new hero

--- Assets/TowerDefensePrototype/Scripts/Battle/Visuals/CameraScaler.cs
+++ Assets/TowerDefensePrototype/Scripts/Battle/Visuals/CameraScaler.cs
@@ -2,8 +2,24 @@ using UnityEngine;
 
 namespace CastlePrototype.Battle.Visuals
 {
-    public class CamereScaler : MonoBehaviour
+    public class CameraScaler : MonoBehaviour
     {
-        
+        [SerializeField] private float minScreenWidth;
+        [SerializeField] private float defaultOrthoSize = 8;
+
+        private void Awake()
+        {
+            float aspect = (float)Screen.width / Screen.height;
+            float screenSizeInWorldUnits = aspect * 2 * Camera.main.orthographicSize;
+
+            if (screenSizeInWorldUnits < minScreenWidth)
+            {
+                Camera.main.orthographicSize = minScreenWidth / (2 * aspect);
+            }
+            else
+            {
+                Camera.main.orthographicSize = defaultOrthoSize;
+            }
+        }
     }
 }
