fa58d81f124e9e836ad057b7bd35bb87796f37b4
Michal Louda <mloud@seznam.cz>
2025-04-10 11:35:11 +0200

    slill manager texts

--- Assets/TowerDefensePrototype/Scripts/Battle/Logic/Managers/Skills/SkillManager.cs
+++ Assets/TowerDefensePrototype/Scripts/Battle/Logic/Managers/Skills/SkillManager.cs
@@ -24,9 +24,9 @@ namespace CastlePrototype.Battle.Logic.Managers.Skills
             {
                 new IncreaseDamageSkill("Increase damage", 25, "Increases damage by {VALUE}%"),
                 new DecreaseAttackInterval("Decrease attack interval", 25, "Decrease attack interval by {VALUE}%"),
-                new IncreaseAttackDistance("Increase attack distance", 25, "Increase attack distance by {VALUE}%"),
-                new IncreaseBounceCountSkill("Increase bounce count", 1, "Increase bounce by {VALUE}%"),
-                new IncreaseFireAgainCountSkill("Increase fire again count", 1, "Increase fire again count by {VALUE}%"),
+                new IncreaseAttackDistance("Increase attack distance", 25, "Increase attack distance by {VALUE}"),
+                new IncreaseBounceCountSkill("Increase bounce count", 1, "Increase bounce by {VALUE}"),
+                new IncreaseFireAgainCountSkill("Increase fire again count", 1, "Increase fire again count by {VALUE}"),
                 new UnlockHeroSkill("Unlock new hero", "soldier", "Add new hero to battle"),
                 new UnlockHeroSkill("Unlock new hero", "turret", "Add new hero to battle"),
                 new UnlockHeroSkill("Unlock new hero", "dron", "Add new hero to battle"),
