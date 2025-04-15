e4ea419e12a32ad6ab7edb4bb469eedb87e522f8
Michal Louda <mloud@seznam.cz>
2025-04-15 13:31:59 +0200

    destroy system fix

--- Assets/TowerDefensePrototype/Scripts/Battle/Logic/Systems/DestroySystem.cs
+++ Assets/TowerDefensePrototype/Scripts/Battle/Logic/Systems/DestroySystem.cs
@@ -45,11 +45,19 @@ namespace CastlePrototype.Battle.Logic.Systems
                     VisualManager.Default.DestroyVisualObject(visualLookup.GetRefRO(entity).ValueRO.VisualIndex);
                 }
 
-                if (teamLoopkup.HasComponent(entity) && teamLoopkup[entity].Team == Team.Enemy && unitLookup.EntityExists(entity))
+                if (teamLoopkup.HasComponent(entity) && teamLoopkup[entity].Team == Team.Enemy && unitLookup.HasComponent(entity))
                 {
                     battlePoints += 1;
                     var battleStatisticComponent = SystemAPI.GetSingleton<BattleStatisticComponent>();
                     battleStatisticComponent.EnemiesKilled++;
+                    Debug.Log($"XXX Destroying entity with def:: {unitLookup[entity].DefinitionId}");
+                    Debug.Log($"XXX Enemies killed {battleStatisticComponent.EnemiesKilled} / {battleStatisticComponent.TotalEnemies}");
+                    if (VisualManager.Default.GetVisualObject(visualLookup.GetRefRO(entity).ValueRO.VisualIndex) == null)
+                    {
+                        Debug.Assert(false ,$"XXX does not gave visual object");
+
+                    }
+                    Debug.Log($"XXX =====");
                     SystemAPI.SetSingleton(battleStatisticComponent);
                 }
 
